f55260aeba8dbb984906f4fb57cf13923173db9a
YuviPanda <yuvipanda@gmail.com>
2016-02-14 18:35:20 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jupyterhub-dummyauthenticator',
+    version='0.1',
+    description='Dummy Authenticator for JupyterHub',
+    url='https://github.com/yuvipanda/jupyterhub-dummyauthenticator',
+    author='Yuvi Panda',
+    author_email='yuvipanda@riseup.net',
+    license='3 Clause BSD',
+    packages=['dummyauthenticator'],
+)
