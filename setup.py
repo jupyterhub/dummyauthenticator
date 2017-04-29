4d2c39fc99a5665691e5d5dbc6156e5f29151e3e
yuvipanda <yuvipanda@gmail.com>
2017-04-29 14:21:19 -0700

    Update author email
    Also 0.2 version didn't seem to 'take' properly, so uploading
    again

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-dummyauthenticator',
-    version='0.2',
+    version='0.3',
     description='Dummy Authenticator for JupyterHub',
     url='https://github.com/yuvipanda/jupyterhub-dummyauthenticator',
     author='Yuvi Panda',
-    author_email='yuvipanda@riseup.net',
+    author_email='yuvipanda@gmail.com',
     license='3 Clause BSD',
     packages=['dummyauthenticator'],
 )
